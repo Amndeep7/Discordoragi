a82d6074092c5e930e2414ecf03052de04dcbfe3
dashwav <dashwav@gmail.com>
2017-08-14 09:06:25 -0700

    cleaned up run script

--- run.py
+++ run.py
@@ -3,22 +3,18 @@ Actually runs the code
 """
 from bot import Discordoragi
 from cogs import Search
-import asyncio
+from asyncio import get_event_loop
 
 
-async def run(future):
-    bot = await Discordoragi.get_bot()
-    search_cog = await Search.create_search(bot)
+def run():
+    loop = get_event_loop()
+    bot = loop.run_until_complete(Discordoragi.get_bot())
+    search_cog = loop.run_until_complete(Search.create_search(bot))
     cogs = [
       search_cog
     ]
-    future.set_result({'bot': bot, 'cogs': cogs})
+    bot.start_bot(cogs)
+
 
 if __name__ == '__main__':
-    loop = asyncio.new_event_loop()
-    future = asyncio.Future()
-    loop.run_until_complete(run(future))
-    bot = future.result()['bot']
-    cogs = future.result()['cogs']
-
-    bot.start_bot(cogs)
+    run()
